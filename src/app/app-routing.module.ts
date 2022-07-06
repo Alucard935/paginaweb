3c229e2b3a61b3b8e7ebcf0a8c5e320c8f9bdf8f
Alucard935 <q.holguinjonathan@gmail.com>
2022-07-05 21:28:03 -0600

    1.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HabitacionComponent } from './habitacion/habitacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "index", component: AppComponent },
+  { path: "habi", component: HabitacionComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/habitacion/habitacion.component.ts
+++ src/app/habitacion/habitacion.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabitacionComponent implements OnInit {
 
+
+  valor = "";
+  json = [
+    { doble: 37, humedad: 10, sensor: "Site 1" },
+    { doble: 27, humedad: 15, sensor: "Site 2" }
+  ];
   constructor() { }
 
   ngOnInit(): void {
