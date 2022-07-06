328e9cfdbf1831df73766c90fc3eb9b73b168e12
CorvusKiller <a1119150053@utch.edu.mx>
2022-07-06 14:52:06 -0600

    Números random  para los sensores

--- src/app/habitacion/habitacion.component.ts
+++ src/app/habitacion/habitacion.component.ts
@@ -5,17 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './habitacion.component.html',
   styleUrls: ['./habitacion.component.css']
 })
+  
 export class HabitacionComponent implements OnInit {
 
-
+  getRandonSensor() {
+    let min = 10;
+    let max = 15;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1) + min)
+  };
   valor = "";
   json = [
-    { doble: 37, humedad: 10, sensor: "Site 1" },
-    { doble: 27, humedad: 15, sensor: "Site 2" }
+    { doble: 37, humedad: this.getRandonSensor(), sensor: "Site 1" },
+    { doble: 27, humedad: this.getRandonSensor(), sensor: "Site 2" }
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
