17a39b819942d562e9c4b8d080e2a92cac977957
Alucard935 <q.holguinjonathan@gmail.com>
2022-07-06 07:56:43 -0600

    1.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HabitacionComponent } from './habitacion/habitacion.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HabitacionComponent
+    HabitacionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
